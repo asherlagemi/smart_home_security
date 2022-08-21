1d60d3f40575c7b7df888978edebeef005554661
Asher Lagemi <asherlagemi@gmail.com>
2022-08-21 14:57:05 +0300

    file changed

--- garden_smart_alarm.py
+++ garden_smart_alarm.py
@@ -7,6 +7,8 @@ from cvlib.object_detection import draw_bbox
 import threading
 import cv2
 
+access_token = 'ACCESS_TOKEN'  # access token from PushBullet
+
 
 class mythread(threading.Thread):
     def __init__(self, thread_name, thread_ID):
@@ -22,10 +24,12 @@ class mythread(threading.Thread):
         return super().is_alive()
 
 
-
 def alarm_sound():
-    playsound('./audio/alarmhome.mp3')
     print('ALERT!!')
+    try:
+        playsound('./audio/alert.mp3')
+    except:
+        print("NO ALARM SOUND IS SET. make sure a file named audio/alarmhome.mp3 exists")
 
 
 def run():
@@ -33,12 +37,15 @@ def run():
     webcam = cv2.VideoCapture(0)
 
     if not webcam.isOpened():
-        print("Could not open webcam")
+        raise Exception("Could not open webcam")
         exit()
 
-    access_token = 'o.CHzTHRGTyTxuQyIJz9IDOTOagjkOebGE'
-    pb = PushBullet(access_token)
-    alarm_id = 1 # index of alarm ID
+    try:
+        pb = PushBullet(access_token)
+    except Exception as e:
+        print(e)
+        print("non valid token")
+    alarm_id = 1  # index of alarm ID
     alarm = mythread('alarm', alarm_id)
     # loop through frames
     while webcam.isOpened():
@@ -53,10 +60,13 @@ def run():
         bbox, label, conf = cv.detect_common_objects(frame, confidence=0.25, model='yolov3-tiny')
         if 'person' in label and not alarm.is_alive():
             alarm = mythread('alarm', ++alarm_id)
-            pb.push_note("ALARM!!!", "someone was seen in your house.")
+            try:
+                pb.push_note("ALARM!!!", "someone was seen in your house.")
+            except:
+                print("push notification wasn't delivered.")
             alarm.start()
 
-        print(bbox, label, conf)
+        # print(bbox, label, conf)
 
         # draw bounding box over detected objects
         out = draw_bbox(frame, bbox, label, conf, write_conf=True)
